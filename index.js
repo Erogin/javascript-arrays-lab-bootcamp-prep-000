8e2460f39ff8ef1d1f2f12db08edd44e76b62e99
Eric Roginek <eroginek@protonmail.com>
2017-09-19 22:29:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -32,5 +32,5 @@ function prependKitten(name){
 
 function removeLastKitten(){
   var length = kittens.length
-  return [...kittens, kittens.slice(0, length)]
+  return [...kittens, kittens.slice(0)]
 }
